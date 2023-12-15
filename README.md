e6f431511de2ba4e3d534fc3b90fc773b1c79771
Melissa Hale <melissakayh@gmail.com>
2023-12-14 19:39:24 -0600

    initial push

---
+++ README.md
@@ -0,0 +1,23 @@
+# Datadog Agent + Node App Example
+
+
+This template deploys a DataDog Agent from the official Docker image as well as a Node Express App with two endpoints that forward metrics and logs to the agent.
+
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template/)
+
+## DataDog Agent
+
+Built from the [Datadog Agent Docker Image](https://hub.docker.com/r/datadog/agent) 
+
+Configuration files -
+- `syslog.yaml` - Configures the Agent to accept syslogs over port 514 forwarded from the node app
+- `datadog.yaml` - Configures the Agent to forward logs to DataDog over HTTP (default is UDP which is not supported in Railway)
+
+## Express App
+
+A very simple express server with two endpoints.
+
+Libraries - 
+
+- [Winston](https://www.npmjs.com/package/winston) & [winston-syslog](https://www.npmjs.com/package/winston-syslog) - Used for forwarding logs to the agent
+- [hot-shots](https://www.npmjs.com/package/hot-shots) - Used for forwarding metrics to the agent
---
+++ expressapi/app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+
+const StatsD = require('hot-shots');
+const { createLogger, format, transports } = require('winston');
+require('winston-syslog').Syslog;
+const port = process.env.PORT || 3000;
+
+// Configure the StatsD client
+console.log("Datadog Agent Host:", process.env.DD_AGENT_HOST);
+console.log("Datadog Agent StatsD Port:", process.env.DD_AGENT_STATSD_PORT);
+const statsdClient = new StatsD({
+  host: process.env.DD_AGENT_HOST,
+  port: process.env.DD_AGENT_STATSD_PORT,
+  protocol: 'udp',
+  cacheDns: true,
+  udpSocketOptions: {
+    type: 'udp6',
+    reuseAddr: true,
+    ipv6Only: true,
+  },
+});
+
+// Configure Winston logger
+const logger = createLogger({
+  level: 'info',
+  exitOnError: false,
+  format: format.json(),
+  transports: [
+    // Syslog transport
+    new transports.Syslog({
+      host: process.env.DD_AGENT_HOST,
+      port: process.env.DD_AGENT_SYSLOG_PORT,
+      protocol: 'udp6',
+      format: format.json(),
+      app_name: 'node-app',
+    }),
+  ],
+});
+
+app.get('/', (req, res) => {
+  // Increment a counter for the root path
+  statsdClient.increment('data_dog_example.homepage.hits');
+  statsdClient.gauge('data_dog_example.homepage.hits', 124);
+
+  // forward logs from root path
+  logger.info('Root route was accessed');
+
+  res.send('Hello World!');
+});
+
+app.get('/test', (req, res) => {
+  // Increment a counter for the test path
+  statsdClient.increment('data_dog_example.testpage.hits');
+
+  // forward logs from test path
+  logger.info('Test route was accessed');
+
+  res.send('This is the test endpoint!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at port ${port}`);
+});
